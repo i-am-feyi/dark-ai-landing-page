66715bd6e3b587a5456cfba00dac55d13a11473b
Feyisayo Badmus <feymus99@gmail.com>
2024-08-07 21:17:13 +0300

    chore: Implement full product showcase section

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import Image from "next/image";
+import productImage from "@/assets/product-image.png";
+import { DotLottiePlayer } from "@dotlottie/react-player";
+
 const tabs = [
   {
     icon: "/assets/lottie/vroom.lottie",
@@ -26,5 +32,45 @@ const tabs = [
 ];
 
 export const Features = () => {
-  return <section>Features</section>;
+  return (
+    <section className="py-20 md:py-24">
+      <div className="container">
+        <div className="text-center max-w-2xl mx-auto">
+          <h2 className="font-medium text-5xl md:text-6xl tracking-tighter leading-tight">
+            Elevate your SEO efforts
+          </h2>
+          <p className="mt-5 text-lg md:text-xl text-white/70 tracking-tight">
+            From small startups to large enterprises, our AI-driven tool has
+            revolutionized the way businesses approach SEO.
+          </p>
+        </div>
+        <div className="flex flex-col lg:flex-row gap-3 mt-10">
+          {tabs.map((tab) => (
+            <div
+              key={tab.title}
+              className="flex items-center gap-2.5 border border-white/15 p-2.5 rounded-xl lg:flex-1"
+            >
+              <div className="flex items-center justify-center size-12 border border-white/15 rounded-xl">
+                <DotLottiePlayer src={tab.icon} className="size-5" autoplay loop />
+              </div>
+              <div className="font-medium">{tab.title}</div>
+              {tab.isNew && (
+                <div className="bg-[#8c44ff] text-black font-semibold text-xs px-2 py-0.5 rounded-full">
+                  New
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+        <div className="border border-white/20 p-2.5 rounded-xl mt-3">
+          <div
+            className="aspect-video bg-cover border border-white/20 rounded-lg"
+            style={{
+              backgroundImage: `url(${productImage.src})`,
+            }}
+          ></div>
+        </div>
+      </div>
+    </section>
+  );
 };
